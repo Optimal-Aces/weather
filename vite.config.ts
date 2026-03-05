1b687a2fce9e3bba9c37ee381accdad9b0d9f9f0
SIGMUND JAMES L. MEJARSE <akosigmund@gmail.com>
2026-03-05 13:21:54 +0800

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,37 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [
+    sveltekit(),
+    SvelteKitPWA({
+      manifest: {
+        name: 'Valencia Weather',
+        short_name: 'Weather',
+        description: 'Local weather app for Valencia City',
+        theme_color: '#071628',
+        background_color: '#071628',
+        display: 'standalone',
+        orientation: 'portrait',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webp}']
+      }
+    })
+  ]
 });
